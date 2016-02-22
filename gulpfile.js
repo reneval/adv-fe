46d427d8f6fafece8765a025139a6de3ca902067
reneval2 <petf@ua.fm>
2016-02-23 01:35:18 +0200

    add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var inject = require('gulp-inject');
 var bower = require('gulp-bower');
-
+var less = require('gulp-less');
+var path = require('path');
+var concat = require('gulp-concat');
+var del = require('del');
 
 gulp.task('bower', function () {
   return bower();
@@ -10,15 +13,24 @@ gulp.task('bower', function () {
 
 
 gulp.task('libs', function () {
-  return gulp.src('libs/**/*.min.js')
-    .pipe(gulp.dest('client_build/libs/'))
+  return gulp.src('./libs/**/*.min.js')
+    .pipe(gulp.dest('./client_build/libs/'))
 
 });
 
-gulp.task('clean', function () {
+gulp.task('clean', function (cb) {
+  del([
+    './client_build/'
+  ], cb);
 });
 
 gulp.task('css', function () {
+  return gulp.src('./styles/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(concat('styles.css'))
+    .pipe((gulp.dest('./client_build/static/')));
 });
 
 
@@ -27,15 +39,16 @@ gulp.task('build', ['copy-static','css'], function () {
 });
 
 gulp.task('copy-static', function () {
-  return  gulp.src('client_src/**/*.{png,jpg,svg,html}')
-  .pipe(gulp.dest('copy-static/'))
+  return  gulp.src('./client_src/**/*.{png,jpg,svg,html}')
+  .pipe(gulp.dest('./copy-static/'))
 });
 
 
 
 
 gulp.task('watch', function () {
-  gulp.watch(['*.html', 'pages/*.html'], ['bs-reload']);
+  gulp.watch(['./client_src/**/*.{png,jpg,svg,html}', './pages/*.html'], ['copy-static']);
+  gulp.watch(['./styles/*.less', './pages/*.html'], ['css']);
 
 });
 
@@ -63,8 +76,8 @@ gulp.task('bs-reload', function () {
 });
 
 
-gulp.task('default', ['libs','build', 'index','browser-sync'], function () {
+gulp.task('default', ['libs','build', 'watch','index','browser-sync'], function () {
   gulp.watch(['*.html', 'pages/*.html'], ['bs-reload']);
   gulp.watch(['*.js', 'js/**/*.js'], ['index', 'bs-reload']);
-  gulp.watch(['*.css', 'css/**/*.css'], ['bs-reload']);
+  gulp.watch(['*.css', 'styles/**/*.css'], ['bs-reload']);
 });
